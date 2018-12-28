9d44ce19f676c771bb64bed819d51e27d966cb35
Andreas Schultz <andreasschultz@Andreass-MBP.fritz.box>
2018-12-28 15:20:24 +0100

    Basic Setup, Page VC Setup

---
+++ Meditation Timer/Meditation Timer/Controller/PageVC.swift
@@ -0,0 +1,119 @@
+//
+//  PageVC.swift
+//  Meditation Timer
+//
+//  Created by Andreas Schultz on 28.12.18.
+//  Copyright © 2018 Andreas Schultz. All rights reserved.
+//
+
+import UIKit
+
+class PageVC: UIPageViewController {
+    
+    //PROPERTIES
+    
+    lazy var orderedViewControllers: [UIViewController] = {
+        return [self.newVc(viewController: "EinfachVC"),
+                self.newVc(viewController: "WaldVC")]
+    }()
+    
+    
+    var pageControl = UIPageControl()
+    
+    //OVERRIDE FUNCS
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        dataSource = self
+        delegate = self
+        
+        // This sets up the first view that will show up on our page control
+        if let firstViewController = orderedViewControllers.first {
+            setViewControllers([firstViewController],
+                               direction: .forward,
+                               animated: true,
+                               completion: nil)
+        }
+        
+        configurePageControl()
+    }
+    
+    //CUSTOM FUNCS
+    
+    func newVc(viewController: String) -> UIViewController {
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: viewController)
+    }
+    
+    func configurePageControl() {
+        // The total number of pages that are available is based on how many available colors we have.
+        pageControl = UIPageControl(frame: CGRect(x: 0,y: UIScreen.main.bounds.maxY - 50,width: UIScreen.main.bounds.width,height: 50))
+        self.pageControl.numberOfPages = orderedViewControllers.count
+        self.pageControl.currentPage = 0
+        self.pageControl.tintColor = UIColor.black
+        self.pageControl.pageIndicatorTintColor = UIColor.white
+        self.pageControl.currentPageIndicatorTintColor = UIColor.black
+        self.view.addSubview(pageControl)
+    }
+    
+    //ACTIONS
+    
+
+}
+
+// EXTENSIONS
+
+extension PageVC: UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        // User is on the first view controller and swiped left to loop to
+        // the last view controller.
+        guard previousIndex >= 0 else {
+            return orderedViewControllers.last
+            // Uncommment the line below, remove the line above if you don't want the page control to loop.
+            // return nil
+        }
+        
+        guard orderedViewControllers.count > previousIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        let orderedViewControllersCount = orderedViewControllers.count
+        
+        // User is on the last view controller and swiped right to loop to
+        // the first view controller.
+        guard orderedViewControllersCount != nextIndex else {
+            return orderedViewControllers.first
+            // Uncommment the line below, remove the line above if you don't want the page control to loop.
+            // return nil
+        }
+        
+        guard orderedViewControllersCount > nextIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[nextIndex]
+    }
+    
+    // MARK: Delegate functions
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        let pageContentViewController = pageViewController.viewControllers![0]
+        self.pageControl.currentPage = orderedViewControllers.index(of: pageContentViewController)!
+    }
+    
+}
